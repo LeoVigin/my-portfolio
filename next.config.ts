2b0095f815864914897ef8cfa543d2b66508a3d5
Leogeox <leovginx@gmail.com>
2026-01-28 10:06:20 +0100

    build: set up sass

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
+import type { NextConfig } from 'next'
+ 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+  sassOptions: {
+    // prependData: `@use "/style/_mixins.scss";`,
+  }
+}
+ 
+export default nextConfig
